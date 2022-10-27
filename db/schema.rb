59188df58b8c0187814e808e5db9910aa625bf84
karnjj <karnjj@hotmail.com>
2022-10-27 14:24:10 +0700

    feat: scaffold inventory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_071617) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_072313) do
+  create_table "inventories", force: :cascade do |t|
+    t.integer "price"
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_inventories_on_item_id"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "stock"
@@ -29,5 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_071617) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "inventories", "items"
+  add_foreign_key "inventories", "users"
   add_foreign_key "items", "users"
 end
