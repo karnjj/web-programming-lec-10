347b24e3c94f68357416a6941eb254990be7faf5
karnjj <karnjj@hotmail.com>
2022-10-27 14:18:10 +0700

    feat: scaffold item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_070432) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_071617) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "stock"
+    t.integer "price"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "login"
@@ -19,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_070432) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "users"
 end
